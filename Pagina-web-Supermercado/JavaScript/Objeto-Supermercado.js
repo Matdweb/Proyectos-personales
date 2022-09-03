6dc4bb128e291dbd17a300e1f4ccc408ee4557b3
Matias Vargas <mat.dweb28[1>
2022-09-03 15:20:50 -0600

    Desarrollo de los objetos "Producto" y "supermercado" 03/09/22   15:20

---
+++ Pagina-web-Supermercado/JavaScript/Objeto-Supermercado.js
@@ -0,0 +1,24 @@
+class Supermercado{
+    constructor(cantidad,total){
+        this.cantidad = cantidad; 
+        this.total = total;
+    }
+
+    //metodos set y get 
+
+    setcantidad(cantidad_){
+        cantidad = cantidad_; 
+    }
+
+    getCantidad(){
+        return this.cantidad; 
+    }
+
+    getTotal(){
+        return this.cantidad; 
+    }
+
+    //metodos Supermaercado
+
+
+}
