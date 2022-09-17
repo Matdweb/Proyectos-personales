186efc11873c293033d5ea56492322052a77aa21
Matias Vargas <mat.dweb28[1>
2022-09-16 22:18:14 -0600

    Desarrollo de la interfaz y de los objetos 16/09/22

---
+++ Tasks-Manager/JavaScript/objtFila.js
@@ -0,0 +1,40 @@
+class Fila{
+    constructor(tarea,encargado,estado,descripcion){
+        this.tarea = tarea; 
+        this.encargado =  encargado; 
+        this.estado = estado;
+        this.descripcion = descripcion;
+    }
+
+    getTarea(){
+        return this.tarea; 
+    }
+
+    getEncargado(){
+        return this.encargado;
+    }
+
+    getEstado(){
+        return this.estado; 
+    }
+
+    getDescripcion(){
+        return this.descripcion; 
+    }
+
+    setTarea(tarea){
+        this.tarea = tarea; 
+    }
+
+    setEncargado(encargado){
+        this.encargado = encargado; 
+    }
+
+    setEstado(estado){
+        this.estado = estado; 
+    }
+
+    setDescripcion(descripcion){
+        this.descripcion = descripcion;
+    }
+}
---
+++ Tasks-Manager/JavaScript/objtTabla.js
@@ -0,0 +1,13 @@
+class Tabla {
+    constructor(filas){
+        this.filas = filas
+    }
+
+    getFilas(){
+        return this.filas;
+    }
+
+    setFilas(filas){
+        this.filas = filas; 
+    }
+}
