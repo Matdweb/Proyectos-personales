d08333db0c784ac15a4117a16c5d1b93fbbc0b8e
Mat dweb <mat.dweb28@gmail.com>
2022-09-17 22:24:32 -0600

    Desarrollo del objt Tabla

--- Tasks-Manager/JavaScript/objtTabla.js
+++ Tasks-Manager/JavaScript/objtTabla.js
@@ -1,3 +1,6 @@
+let ContTareas = document.querySelector(".container-tareas"); 
+let ArregloTablas = []; 
+
 class Tabla {
     constructor(filas){
         this.filas = filas
@@ -10,4 +13,19 @@ class Tabla {
     setFilas(filas){
         this.filas = filas; 
     }
+
+    NuevaTabla(){
+        ArregloTablas.push(new Tabla(1));
+        ContTareas.innerHTML = `<div class="tabla">
+                                    <div class="fila">
+                                        <label>Tarea</label>
+                                        <label>Encargado</label>
+                                        <label>Estado</label>
+                                        <label>Descripcion</label>
+                                    </div>
+                                </div>`;
+        console.log(ArregloTablas);
+    }
 }
+
+ArregloTablas.push(new Tabla(4));
