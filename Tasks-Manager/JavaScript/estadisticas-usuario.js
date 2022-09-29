ae38ab19c45e643464bc05c6713f7b7be7ace0ce
Mat dweb <mat.dweb28@gmail.com>
2022-09-29 15:57:37 -0600

    Desarrollo del metodo efectividadUsuario exitoso 29/09/22    15:57

--- Tasks-Manager/JavaScript/estadisticas-usuario.js
+++ Tasks-Manager/JavaScript/estadisticas-usuario.js
@@ -2,7 +2,7 @@ const todasTareas = () =>{
     let tablas = document.querySelectorAll('.tabla');
     let tareas = 0; 
     for(var tabla of tablas){
-       tareas += tabla.children.length-1
+       tareas += tabla.children.length-1  //se resta 1 debido a que la primera fila no cuenta
     }
     console.log(tareas)
     return tareas;
@@ -29,3 +29,31 @@ const tareasXRealizar = () =>{
 }
 
 tareasXRealizar();
+
+const efectividadUsuario = () =>{
+    let tablas = document.querySelectorAll('.tabla');
+    let listas = 0; 
+    for(var tabla of tablas){
+        for(var i=1; i<tabla.children.length; i++){
+            if(tabla.children[i].children[2].firstElementChild.firstElementChild.innerHTML=='Listo' 
+            || tabla.children[i].children[2].firstElementChild.firstElementChild.innerHTML=='Casi Listo'){
+                listas++;
+            }
+        }
+    }
+    console.log(listas);
+    return todasTareas() - listas
+}
+
+const Responsabilidad = () =>{
+    let tablas = document.querySelectorAll('.tabla');
+    let hoy = new Date; 
+    let faltas = 0; //variable que almacena el numero de tareas que expiraron
+    for(var tabla of tablas){
+        for(var i=1; i<tabla.children.length; i++){
+        let fecha = tabla.children[i].querySelector(`#fecha`).value;
+       console.log(fecha.split('-').join(',')) 
+        }
+     }
+
+}
